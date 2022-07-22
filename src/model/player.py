b837b2c98d0cf39f71d966566a1bb2e86b0c6393
Dany Corleone <corleonedany@live.fr>
2022-07-23 01:27:45 +0200

    formated file with pylama

--- src/model/player.py
+++ src/model/player.py
@@ -1,15 +1,8 @@
 class Player:
-    def __init__(
-            self,
-            first_name,
-            last_name,
-            date_of_birth,
-            sex,
-            rank=0,
-            score=0,
-            doc_id=None,
-            players_faced=[],
-    ):
+    def __init__(self, first_name, last_name, date_of_birth,
+                 sex, rank=0, score=0, doc_id=None, players_faced=None):
+        if players_faced is None:
+            players_faced = []
         self.firstName = first_name
         self.lastName = last_name
         self.date_of_birth = date_of_birth
