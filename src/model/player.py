01230144d65512e41a62bb5fdb34fd29d116cdea
CorleoneDany <corleonedany@live.fr>
2022-07-23 01:20:15 +0200

    First commit

---
+++ src/model/player.py
@@ -0,0 +1,20 @@
+class Player:
+    def __init__(
+            self,
+            first_name,
+            last_name,
+            date_of_birth,
+            sex,
+            rank=0,
+            score=0,
+            doc_id=None,
+            players_faced=[],
+    ):
+        self.firstName = first_name
+        self.lastName = last_name
+        self.date_of_birth = date_of_birth
+        self.sex = sex
+        self.rank = int(rank) or 0
+        self.score = int(score) or 0
+        self.doc_id = doc_id
+        self.players_faced = players_faced
