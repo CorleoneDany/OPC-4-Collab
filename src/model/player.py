4c5d7657792db92215af16c0ab81878b8fd47bce
CorleoneDany <corleonedany@live.fr>
2022-07-23 02:23:37 +0200

    Debug method __tostring__

--- src/model/player.py
+++ src/model/player.py
@@ -1,3 +1,6 @@
+from faker import Faker
+
+
 class Player:
     def __init__(self, first_name, last_name, date_of_birth,
                  sex, rank=0, score=0, doc_id=None, players_faced=None):
@@ -11,3 +14,27 @@ class Player:
         self.score = int(score) or 0
         self.doc_id = doc_id
         self.players_faced = players_faced
+
+    def __str__(self) -> str:
+        return (f"{self.first_name} {self.last_name}")
+
+    @classmethod
+    def createPlayer(self):
+        faker = Faker()
+        players = []
+        for i in range(8):
+            profile = faker.profile()
+            first_name, last_name = profile["name"].split(" ", 1)
+            params = {
+                "first_name": first_name,
+                "last_name": last_name,
+                "date_of_birth": profile["birthdate"].strftime("%d/%m/%Y"),
+                "sex": profile["sex"]
+            }
+            player = Player(**params)
+            players.append(player)
+        return players
+
+
+players = Player.createPlayer()
+print(players)
